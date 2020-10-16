ac4832d884a176416c8328f1f53403f14a590a81
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-10-16 21:12:51 +0200

    Update cleanmem.asm

--- src/01-Clean-Memory/cleanmem.asm
+++ src/01-Clean-Memory/cleanmem.asm
@@ -11,10 +11,8 @@ Start:
     ldx #$FF        ; loads the X register with #$FF
     txs             ; transfer X register to S(tack) register
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Clear the Zero Page region ($00 to $FF)
 ; Meaning the entire TIA register space and also RAM
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     lda #0          ; A = 0
     ldx #$FF        ; X = #$FF
 
@@ -23,9 +21,7 @@ MemLoop:
     dex             ; x--
     bne MemLoop     ; loop until X==0 (z-flag set)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; File ROM size to exactly 4KB
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     org $FFFC
     .word Start     ; reset vector at $FFFC (where program starts)
     .word Start	    ; interrupt vector at $FFFE (unused in VCS)
